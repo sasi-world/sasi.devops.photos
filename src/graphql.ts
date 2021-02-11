d158ea3e95f8e636811950e1a11ce1ffb661ef4c
AmoDinho <u14284783@tuks.co.za>
2021-02-11 22:32:25 +0200

    added introspection true

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,37 +1,42 @@
 import { ApolloServer, gql } from "apollo-server-lambda";
-import { ITypeDefinitions } from "graphql-tools/dist/Interfaces"
-import { Context, Callback, APIGatewayEvent } from "aws-lambda"
+import { ITypeDefinitions } from "graphql-tools/dist/Interfaces";
+import { Context, Callback, APIGatewayEvent } from "aws-lambda";
 import { schema } from "./schema";
 import { resolvers } from "./resolvers";
 
 //const typeDef: ITypeDefinitions = schema
 const server = new ApolloServer({
-	typeDefs: schema,
-	resolvers: resolvers,
-	formatError: error => {
-		return error;
-	},
-	formatResponse: (response: any) => {
-		return response;
-	},
-	context: ({ event, context }) => ({
-		headers: event.headers,
-		functionName: context.functionName,
-		event,
-		context
-	}),
-	tracing: true,
-	playground: true
+  typeDefs: schema,
+  resolvers: resolvers,
+  formatError: (error) => {
+    return error;
+  },
+  formatResponse: (response: any) => {
+    return response;
+  },
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+  tracing: true,
+  playground: true,
+  introspection: true,
 });
 
-exports.graphqlHandler = (event: APIGatewayEvent, context: Context, callback: Callback) => {
-	const handler = server.createHandler({
-		cors: {
-			origin: "*",
-			credentials: true,
-			methods: ["POST", "GET"],
-			allowedHeaders: ["Content-Type", "Origin", "Accept"]
-		}
-	});
-	return handler(event, context, callback);
+exports.graphqlHandler = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback
+) => {
+  const handler = server.createHandler({
+    cors: {
+      origin: "*",
+      credentials: true,
+      methods: ["POST", "GET"],
+      allowedHeaders: ["Content-Type", "Origin", "Accept"],
+    },
+  });
+  return handler(event, context, callback);
 };
