0785b55872df4ff2a6bdf076e99e74a0c3abbe80
AmoDinho <u14284783@tuks.co.za>
2021-02-11 22:24:29 +0200

    created getall func

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,3 +1,7 @@
 export const hello = (args: any, context: any) => {
-    return "Your GraphQL API is now LIVE!🎈 "
-}
+  return "Your GraphQL API is now LIVE!🎈 ";
+};
+
+export const getAllPhotos = (args: any, contetxt: any) => {
+  return null;
+};
